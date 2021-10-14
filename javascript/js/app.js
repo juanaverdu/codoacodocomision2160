3e97f1fe5eb5c3690a8b5fe426155013ba2134fc
juana verdu <j.v.juanaverdu@gmail.com>
2021-10-14 19:28:15 -0300

    js variables tipos condicionales

---
+++ javascript/js/app.js
@@ -0,0 +1,18 @@
+alert('hola aletra jeje');
+        console.log('consola jeje');
+        /*Variables*/
+        var nombreVariable;
+        const pi = 3.14159;
+        let variableTres = 'Hola';
+        let hayClases = true;
+        /*Condicionales*/
+        if(hayClases){
+            console.log('estudiar')
+        }else{
+            console.log('dormir')
+        }
+
+        let elemento = document.getElementById("principal");
+        elemento.innerHTML = "gholdfs"; 
+        elemento.innerHTML += "<p>" + variableTres + "</p>";
+        document.write("<h3> hola asdadsa dfsfgda </h3>")
